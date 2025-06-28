d6dcd1ba59fae5e840faa3c6e5415c0d9788ea58
Harsh Palod <156534599+harhspalod@users.noreply.github.com>
2025-06-29 00:50:40 +0530

    Fix cookies() requestAsyncStorage Error

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,29 +1,9 @@
-import { cookies } from "next/headers";
+import SidebarWrapper from "./_components/SidebarWrapper";
 
-import Header from "@/components/shared/header";
-import Container from "@/components/ui/container";
-import AppSidebar from "@/components/shared/AppSidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
-
-  return (
-    <SidebarProvider defaultOpen={defaultOpen}>
-      <AppSidebar />
-
-      <div className="w-full relative overflow-y-auto">
-        <Header />
-
-        <main className="pt-6 pb-8">
-          <Container>{children}</Container>
-        </main>
-      </div>
-    </SidebarProvider>
-  );
+  return <SidebarWrapper>{children}</SidebarWrapper>;
 }
